70731fb5a4a85001ce6dee11dc7dacc9e56202a6
RadoHarilala <118272464+RadoHarilalaEtech@users.noreply.github.com>
2022-11-15 15:20:07 +0300

    add update to done

--- main.py
+++ main.py
@@ -5,18 +5,27 @@ from jira import JIRA
 from jira.client import ResultList
 from jira.resources import Issue
 
-user = ""
-pwd = ""
+USER = ""
+PWD = ""
+
+id_fini= "31"
 # Some Authentication Methods
 jira = JIRA(
-    basic_auth=(user, pwd),  # a username/password tuple [Not recommended]
+    basic_auth=(USER, PWD),  # a username/password tuple [Not recommended]
     # basic_auth=("email", "API token"),  # Jira Cloud: a username/token tuple
     # token_auth="API token",  # Self-Hosted Jira (e.g. Server): the PAT token
     # auth=("admin", "admin"),  # a username/password tuple for cookie auth [Not recommended]
     server="https://jira.arkeup.com/"
 )
-jira.search_issues('project = F2F AND status in ("En attente de validation", "In Progress", "To Do") AND assignee in (currentUser())')
+
+def update_to_done(issu_list=[]):
+    for issue in issu_list:
+        name = issue.key
+        jira.transition_issue(name, id_fini)
+        print(f"MAJ: {name} OK")
+        break
+
 if __name__ == "__main__":
-    myself = jira.myself()
-    issues = cast(ResultList[Issue], jira.search_issues("assignee=me"))
-    print(issues)
+    all = jira.search_issues('project = F2F AND status in ("En attente de validation", "In Progress", "To Do") AND assignee in (currentUser())')
+    print(f"Found {len(all)} tickets")
+    update_to_done(all)
